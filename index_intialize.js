9c980311df67d67ef0da15a98d42f634d122ab18
Tina Bryson <tinabryson@tinas-mbp.attlocal.net>
2017-11-18 12:45:40 -0500

    fixed intial bugs in initialize and add start command package.json

--- index_intialize.js
+++ index_intialize.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('postgres://patrick@localhost:5432/shoppinghero');
+sequelize = new Sequelize('postgres://tinabryson@localhost:5432/shoppinghero_db');
 
 sequelize
   .authenticate()
@@ -13,19 +12,21 @@ sequelize
 
 const Person = sequelize.define('person', {
   id: {
-    type: Sequelize.INTERGER
+    type: Sequelize.INTEGER,
+    primaryKey: true
   },
   name: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING(30)
   }
 });
 
 const Items = sequelize.define('items', {
   id: {
-    type: Sequelize.INTERGER
+    type: Sequelize.INTEGER,
+    primaryKey: true
   },
   name: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING(30)
   },
   person_id: {
     type: Sequelize.INTEGER
@@ -36,7 +37,7 @@ Person.sync({force: true}).then(() => {
   return Person.create({
     id: 1,
     name: 'TestPerson_1'
-  })
+  });
 });
 
 Items.sync({force: true}).then(() => {
@@ -44,5 +45,5 @@ Items.sync({force: true}).then(() => {
     id: 1,
     name: 'TestItem_1',
     person_id: 1
-  })
+  });
 });
