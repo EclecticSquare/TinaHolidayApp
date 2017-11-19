9148eeb037bd7bced6b5f98e3c5ca0a9cb492020
Tina Bryson <tinabryson@tinas-mbp.attlocal.net>
2017-11-18 20:17:02 -0500

    post working with postman

--- requests.js
+++ requests.js
@@ -1,17 +1,56 @@
-// GET REQUEST ------------------------------
+
+
+const Sequelize = require('sequelize');
+sequelize = new Sequelize('postgres://tinabryson@localhost:5432/shoppinghero_db');
+
 
 let express = require('express');
 let bodyParser = require('body-parser');
 let url = require('url');
 
 let app = express()
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //takes the binary and changes it into language we can understand
+
+//Shows want an instance for each table should look like
+const Person = sequelize.define('person', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING(30)
+  }
+});
+
+const Items = sequelize.define('items', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING(30)
+  },
+  person_id: {
+    type: Sequelize.INTEGER
+  }
+});
 
 
-// STATUS
-app.get('/status', (request, response) => {
-    response.send('{"status": "on" }')
-})
+//Takes information given from the user and inserts into the table/database/model
+app.post('/person/create', (request, response) => {
+  let newPerson = request.body
+  Person.create(
+    newPerson
+  ).then((results) => {
+    response.json(results)
+  });
+});
+
+
+
+
+
+
 
 // TEST!!!!
 // app.get('/test', (request, response) => {
@@ -21,7 +60,7 @@ app.get('/status', (request, response) => {
 
 // tell app what port to listen for
 app.listen(3000, () => {
-  console.log('Listening on port 3000')
+  console.log('I can hear you!')
 })
 
 // ---------------------------------------------
